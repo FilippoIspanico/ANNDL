9ddec2b72b1dabaa19e0495d80583ec7a3fe335a
ettoremodina <modinaettore@outlook.it>
2023-11-05 23:23:13 +0100

    risolto il problema librerie

--- Homework1/function_folder/librarys.py
+++ Homework1/function_folder/librarys.py
@@ -1,39 +1,47 @@
-def librarys_upload():
-    seed = 42
-    import os
+print("librerie in caricamento")
+
+seed = 42
+import os
 
 
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    os.environ['MPLCONFIGDIR'] = os.getcwd()+'/configs/'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['PYTHONHASHSEED'] = str(seed)
+os.environ['MPLCONFIGDIR'] = os.getcwd()+'/configs/'
 
-    import warnings
-    warnings.simplefilter(action='ignore', category=FutureWarning)
-    warnings.simplefilter(action='ignore', category=Warning)
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=Warning)
 
-    import numpy as np
-    np.random.seed(seed)
+import numpy as np
+np.random.seed(seed)
 
-    import logging
+import logging
 
-    import random
-    random.seed(seed)
+import random
+random.seed(seed)
 
-    # Import tensorflow
-    import tensorflow as tf
-    from tensorflow import keras as tfk
-    from tensorflow.keras import layers as tfkl
-    tf.autograph.set_verbosity(0)
-    tf.get_logger().setLevel(logging.ERROR)
-    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-    tf.random.set_seed(seed)
-    tf.compat.v1.set_random_seed(seed)
-    print(tf.__version__)
+# Import tensorflow
+import tensorflow as tf
+from tensorflow import keras as tfk
+from tensorflow.keras import layers as tfkl
+tf.autograph.set_verbosity(0)
+tf.get_logger().setLevel(logging.ERROR)
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+tf.random.set_seed(seed)
+tf.compat.v1.set_random_seed(seed)
+print(tf.__version__)
 
-    # Import other libraries
-    import cv2
-    from tensorflow.keras.applications.mobilenet import preprocess_input
-    import matplotlib.pyplot as plt
-    from sklearn.model_selection import train_test_split
-    from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
-    import seaborn as sns
+# Import other libraries
+#import cv2
+from tensorflow.keras.applications.mobilenet import preprocess_input
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
+import seaborn as sns
+
+
+import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator, NumpyArrayIterator
+from PIL import Image
+
+print("librerie caricate")
